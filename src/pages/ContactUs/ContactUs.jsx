6be1e0927c2d14ba6e2203e621b7d4f18d79a558
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2021-02-20 22:11:17 +0530

    update

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import "./ContactUs.css";
-import Logo from "../../components/Logo/Logo";
+import React, { Component } from 'react'
+import './ContactUs.css'
+import Logo from '../../components/Logo/Logo'
 
 class ContactUs extends Component {
   render() {
@@ -8,46 +8,30 @@ class ContactUs extends Component {
       <>
         <Logo />
 
-        <div className="contact">
-          <h1 className="Contact-us">Contact Us</h1>
-          <div className="info">
-            <div className="profile-card">
+        <div className='contact'>
+          <h1 className='Contact-us'>Contact Us</h1>
+          <div className='info'>
+            <div className='profile-card'>
               <img
-                src={require("../../assets/img/lakshayimg.jpg")}
-                alt="Lakshay"
-                className="lakshay-img"
+                src={require('../../assets/img/IMG20200207175040.jpg')}
+                alt='Akshat'
+                className='akshat-img'
               />
-              <h3 className="profile-name">Lakshay Saini</h3>
-              <p className="information">Frontend Developer</p>
-              <a href="https://www.linkedin.com/in/lakshay-saini-dev/">
+              <h3 className='profile-name'>Akshat Austin</h3>
+              <p className='information'>Frontend Developer</p>
+              <a href='https://www.linkedin.com/in/akshat-austin/'>
                 <img
-                  src={require("../../assets/img/icons8-linkedin-480.png")}
-                  alt="Linkedin-profile"
-                  className="linkedin"
-                />
-              </a>
-            </div>
-            <div className="profile-card">
-              <img
-                src={require("../../assets/img/IMG20200207175040.jpg")}
-                alt="Akshat"
-                className="akshat-img"
-              />
-              <h3 className="profile-name">Akshat Austin</h3>
-              <p className="information">Frontend Developer</p>
-              <a href="https://www.linkedin.com/in/akshat-austin/">
-                <img
-                  src={require("../../assets/img/icons8-linkedin-480.png")}
-                  alt="Linkedin-profile"
-                  className="linkedin"
+                  src={require('../../assets/img/icons8-linkedin-480.png')}
+                  alt='Linkedin-profile'
+                  className='linkedin'
                 />
               </a>
             </div>
           </div>
         </div>
       </>
-    );
+    )
   }
 }
 
-export default ContactUs;
+export default ContactUs
